51c6525c6791b9b44e30a2053926ac893e530f40
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-04-18 13:51:56 +0200

    MainImage drags opposite directions to mouse movement

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import "./styles/Leaderboard.css";
+import { Link } from "react-router-dom";
 
 function Leaderboard({ players, correctPlayerIndex }) {
   return (
@@ -20,6 +21,12 @@ function Leaderboard({ players, correctPlayerIndex }) {
           </span>
         </div>
       ))}
+      <span>You rank: {correctPlayerIndex + 1}</span>
+      <Link to="/">
+        <button type="button" className="go-home-btn">
+          Go Home
+        </button>
+      </Link>
     </div>
   );
 }
--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+// import React from "react";
+// import waldo from "../images/waldo.jpg";
+
+// function MainImage() {
+//   return (
+//     <div className="main-img-wrapper">
+//       <img src={waldo} alt="All characters." className="main" />
+//     </div>
+//   );
+// }
+
+// export default MainImage;
+import React, { useRef, useEffect } from "react";
 import waldo from "../images/waldo.jpg";
+import "./styles/MainImage.css";
 
 function MainImage() {
+  const wrapperRef = useRef(null);
+  let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
+    wrapper.addEventListener("mouseenter", startDrag);
+    wrapper.addEventListener("mouseleave", stopDrag);
+    wrapper.addEventListener("mousemove", drag);
+
+    return () => {
+      wrapper.removeEventListener("mouseenter", startDrag);
+      wrapper.removeEventListener("mouseleave", stopDrag);
+      wrapper.removeEventListener("mousemove", drag);
+    };
+  });
+
+  const startDrag = (e) => {
+    const wrapper = wrapperRef.current;
+    pos = {
+      left: wrapper.scrollLeft,
+      top: wrapper.scrollTop,
+      x: e.clientX,
+      y: e.clientY,
+    };
+    wrapper.style.cursor = "grabbing";
+  };
+
+  const stopDrag = () => {
+    const wrapper = wrapperRef.current;
+    wrapper.style.cursor = "grab";
+  };
+
+  const drag = (e) => {
+    const wrapper = wrapperRef.current;
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    if (dx !== 0 || dy !== 0) {
+      wrapper.scrollLeft = pos.left + dx;
+      wrapper.scrollTop = pos.top + dy;
+    }
+
+    pos = {
+      left: wrapper.scrollLeft,
+      top: wrapper.scrollTop,
+      x: e.clientX,
+      y: e.clientY,
+    };
+  };
+
   return (
-    <div className="main-img-wrapper">
+    <div className="main-img-wrapper" ref={wrapperRef}>
       <img src={waldo} alt="All characters." className="main" />
     </div>
   );

6ec248aac96d2d92bd0dce1ab32cb14b3d2e13f3
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-04-18 09:37:48 +0200

    ImageGallery fetches charaters properly

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,34 +1,36 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
-import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 function ImageGallery({ firebaseApp }) {
   const [imageUrls, setImageUrls] = useState([]);
   const storage = getStorage(firebaseApp);
+  const charList = [
+    "Jerry_Mouse.png",
+    "sonic-png-11.png",
+    "Gay_rabbit_max.webp",
+  ];
 
   useEffect(() => {
-    const storageRef = ref(storage);
+    const fetchImageUrls = async () => {
+      // Clear imageUrls array
+      setImageUrls([]);
 
-    listAll(storageRef)
-      .then((result) => {
-        console.log(result);
-        result.items.forEach((imageRef) => {
-          getDownloadURL(imageRef).then((url) => {
-            if (!imageUrls.includes(url)) {
-              setImageUrls((prevUrls) => [...prevUrls, url]);
-            }
-          });
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      // Fetch image URLs and update state
+      const promises = charList.map((char) =>
+        getDownloadURL(ref(storage, char))
+      );
+      const urls = await Promise.all(promises);
+      setImageUrls(urls);
+    };
+
+    fetchImageUrls();
   }, []);
 
   return (
     <div className="gallery">
       {imageUrls.map((url, index) => (
-        <img key={url} src={url} alt={`${index}`} />
+        <img key={url} src={url} alt={index} />
       ))}
     </div>
   );

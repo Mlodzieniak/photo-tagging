17af7c82351aeac1203260b3437f57c2f5faa094
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-04-20 11:50:00 +0200

    popup translates position together with MainImage

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -6,14 +6,13 @@ import waldo from "../images/waldo.jpg";
 import "./styles/MainImage.css";
 
 function MainImage() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 });
   const wrapperRef = useRef(null);
+  const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 });
 
   const drag = (e) => {
     const wrapper = wrapperRef.current;
 
-    // calculate position of mouse relative to center of wrapper
+    // calculate position of mouse relative to top-left of wrapper
     const wrapperRect = wrapper.getBoundingClientRect();
     const mouseX = e.clientX - wrapperRect.left;
     const mouseY = e.clientY - wrapperRect.top;
@@ -25,40 +24,19 @@ function MainImage() {
       (imgRect.width - wrapperRect.width) * (mouseX / wrapperRect.width);
     const imgY =
       (imgRect.height - wrapperRect.height) * (mouseY / wrapperRect.height);
-
     img.style.transform = `translate(${-imgX}px, ${-imgY}px)`;
-    setMousePosition({ x: mouseX, y: mouseY });
+    setPopupPosition({ x: imgX, y: imgY });
   };
-
-  // const calcMousePosition = (e) => {
-  //   const wrapper = wrapperRef.current;
-  //   const wrapperRect = wrapper.getBoundingClientRect();
-  //   const mouseX = e.clientX - wrapperRect.left;
-  //   const mouseY = e.clientY - wrapperRect.top;
-  //   // setMousePosition({ x: mouseX, y: mouseY });
-  // };
 
   useEffect(() => {
     const wrapper = wrapperRef.current;
-
     wrapper.addEventListener("mousemove", drag);
-    // wrapper.addEventListener("click", calcMousePosition);
-
     return () => {
       wrapper.removeEventListener("mousemove", drag);
-      // wrapper.removeEventListener("click", calcMousePosition);
     };
   });
-  const handleClick = () => {
-    const wrapper = wrapperRef.current;
-    const img = wrapper.querySelector("img.main");
-    const imgRect = img.getBoundingClientRect();
-    const popup = wrapper.querySelector(".popup-container");
-    const popupRect = popup.getBoundingClientRect();
-    const popupX = imgRect.left + mousePosition.x - popupRect.width / 2;
-    const popupY = imgRect.top + mousePosition.y - popupRect.height / 2;
-    setPopupPosition({ x: popupX, y: popupY });
-  };
+
+  const handleClick = () => {};
   // useEffect(() => {
   //   const wrapper = wrapperRef.current;
   //   const img = wrapper.querySelector("img.main");
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import propTypes from "prop-types";
 import "./styles/Popup.css";
 
 // window with 3 buttons that pops up in position that is received from props
 function Popup({ newPosition }) {
-  const [position, setPosition] = useState(newPosition);
+  // const [position, setPosition] = useState(newPosition);
+  const wrapper = useRef(null);
+  // console.log(wrapper.current);
+  // wrapper.current.style.transform = "translate(300px, 450px)";
+  wrapper.current.style.transform = `translate(${-newPosition.x}px, ${-newPosition.y}px)`;
 
-  const handleClose = () => {
-    setPosition({ x: 0, y: 0 });
-  };
+  // const handleClose = () => {
+  //   setPosition({ x: 0, y: 0 });
+  // };
 
   return (
-    <div className="popup-container">
+    <div className="popup-container" ref={wrapper}>
       {/* {position.x !== 0 || position.y !== 0 || (
         <div className="popup" style={{ top: position.y, left: position.x }}>
           <button type="button">Button 1</button>
@@ -23,11 +27,12 @@ function Popup({ newPosition }) {
           </button>
         </div>
       )} */}
-      <div className="popup" style={{ top: position.y, left: position.x }}>
+      <div className="popup" style={{ top: 300, left: 20 }}>
+        {/* <div className="popup"> */}
         <button type="button">Button 1</button>
         <button type="button">Button 2</button>
         <button type="button">Button 3</button>
-        <button type="button" className="close-button" onClick={handleClose}>
+        <button type="button" className="close-button">
           X
         </button>
       </div>

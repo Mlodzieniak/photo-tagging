0de1895507f55d28d180db586811ecbf43cec6bb
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-04-25 19:46:13 +0200

    change firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  updateDoc,
-  doc,
-  getDoc,
-} from "firebase/firestore";
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  // useNavigate,
-} from "react-router-dom";
+import { collection, updateDoc, doc, getDoc } from "firebase/firestore";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { db, firebaseApp } from "./firebase";
 import Game from "./components/Game";
 import WelcomePage from "./components/WelcomePage";
 import Leaderboard from "./components/Leaderboard";
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyABksPd_kbV_p3647KCEEkj1yjgr4Hgkmc",
-  authDomain: "photo-tagging-app-be03b.firebaseapp.com",
-  projectId: "photo-tagging-app-be03b",
-  storageBucket: "photo-tagging-app-be03b.appspot.com",
-  messagingSenderId: "396255562252",
-  appId: "1:396255562252:web:6f5909cfe719802d263c29",
-  measurementId: "G-R21BFDEG1T",
-});
-
 function App() {
   const [player, setPlayer] = useState(null);
   const [time, setTime] = useState(null);
-  const db = getFirestore(firebaseApp);
 
   const setPlayerName = (name) => {
     setPlayer(name);
